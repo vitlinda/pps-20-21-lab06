bdb6cf2c4aa288375ef733925a2f840e4f612460
Linda Vitali <linda.vitali2@studio.unibo.it>
2021-04-21 12:54:51 +0200

    comment restored

--- src/u06lab/code/Combiner.scala
+++ src/u06lab/code/Combiner.scala
@@ -33,6 +33,19 @@ object FunctionsImpl extends Functions {
 
 }
 
+/*
+  * 2) To apply DRY principle at the best,
+  * note the three methods in Functions do something similar.
+  * Use the following approach:
+  * - find three implementations of Combiner that tell (for sum,concat and max) how
+  *   to combine two elements, and what to return when the input list is empty
+  * - implement in FunctionsImpl a single method combiner that, other than
+  *   the collection of A, takes a Combiner as input
+  * - implement the three methods by simply calling combiner
+  *
+  * When all works, note we completely avoided duplications..
+ */
+
 trait Combiner[A] {
   def unit: A
   def combine(a: A, b: A): A
