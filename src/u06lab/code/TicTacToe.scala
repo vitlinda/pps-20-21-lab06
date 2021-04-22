0610188c6dd1228e5374edee7bcb6a20642f4137
Linda Vitali <linda.vitali2@studio.unibo.it>
2021-04-22 15:27:58 +0200

    test find

--- src/u06lab/code/TicTacToe.scala
+++ src/u06lab/code/TicTacToe.scala
@@ -12,7 +12,11 @@ object TicTacToe {
   type Board = List[Mark]
   type Game = List[Board]
 
-  def find(board: Board, x: Int, y: Int): Option[Player] = ???
+  def find(board: Board, x: Int, y: Int): Option[Player] = board match {
+    case Nil => None
+    case h :: _ if(h.x == x && h.y == y) => Some(h.player)
+    case _ :: t => find(t, x , y)
+  }
 
   def placeAnyMark(board: Board, player: Player): Seq[Board] = ???
 
@@ -24,31 +28,4 @@ object TicTacToe {
       if (x == 2) { print(" "); if (board == game.head) println()}
     }
 
-  // Exercise 1: implement find such that..
-  println(find(List(Mark(0,0,X)),0,0)) // Some(X)
-  println(find(List(Mark(0,0,X),Mark(0,1,O),Mark(0,2,X)),0,1)) // Some(O)
-  println(find(List(Mark(0,0,X),Mark(0,1,O),Mark(0,2,X)),1,1)) // None
-
-  // Exercise 2: implement placeAnyMark such that..
-  printBoards(placeAnyMark(List(),X))
-  //... ... ..X ... ... .X. ... ... X..
-  //... ..X ... ... .X. ... ... X.. ...
-  //..X ... ... .X. ... ... X.. ... ...
-  printBoards(placeAnyMark(List(Mark(0,0,O)),X))
-  //O.. O.. O.X O.. O.. OX. O.. O..
-  //... ..X ... ... .X. ... ... X..
-  //..X ... ... .X. ... ... X.. ...
-
-  // Exercise 3 (ADVANCED!): implement computeAnyGame such that..
-  computeAnyGame(O, 4) foreach {g => printBoards(g); println()}
-  //... X.. X.. X.. XO.
-  //... ... O.. O.. O..
-  //... ... ... X.. X..
-  //              ... computes many such games (they should be 9*8*7*6 ~ 3000).. also, e.g.:
-  //
-  //... ... .O. XO. XOO
-  //... ... ... ... ...
-  //... .X. .X. .X. .X.
-
-  // Exercise 4 (VERY ADVANCED!) -- modify the above one so as to stop each game when someone won!!
 }
---
+++ test/u06lab/code/TicTacToeTest.scala
@@ -0,0 +1,46 @@
+package u06lab.code
+
+import org.junit.jupiter.api.Assertions._
+import org.junit.jupiter.api.Test
+import u06lab.code.TicTacToe.{Mark, O, X, computeAnyGame, find, placeAnyMark, printBoards}
+
+class TicTacToeTest {
+
+  @Test
+  def testFind = {
+    assertEquals(Some(X), find(List(Mark(0,0,X)), 0, 0))
+    assertEquals(Some(O), find(List(Mark(0,0,X),Mark(0,1,O),Mark(0,2,X)), 0, 1))
+    assertEquals(None, find(List(Mark(0,0,X),Mark(0,1,O),Mark(0,2,X)), 1, 1))
+  }
+
+  @Test
+  def testPlaceAnyMark = {
+    // Exercise 2: implement placeAnyMark such that..
+    printBoards(placeAnyMark(List(),X))
+    //... ... ..X ... ... .X. ... ... X..
+    //... ..X ... ... .X. ... ... X.. ...
+    //..X ... ... .X. ... ... X.. ... ...
+    printBoards(placeAnyMark(List(Mark(0,0,O)),X))
+    //O.. O.. O.X O.. O.. OX. O.. O..
+    //... ..X ... ... .X. ... ... X..
+    //..X ... ... .X. ... ... X.. ...
+  }
+
+  @Test
+  def testComputeAnyGames = {
+    // Exercise 3 (ADVANCED!): implement computeAnyGame such that..
+    computeAnyGame(O, 4) foreach {g => printBoards(g); println()}
+    //... X.. X.. X.. XO.
+    //... ... O.. O.. O..
+    //... ... ... X.. X..
+    //              ... computes many such games (they should be 9*8*7*6 ~ 3000).. also, e.g.:
+    //
+    //... ... .O. XO. XOO
+    //... ... ... ... ...
+    //... .X. .X. .X. .X.
+  }
+
+  // Exercise 4 (VERY ADVANCED!) -- modify the above one so as to stop each game when someone won!!
+
+
+}
